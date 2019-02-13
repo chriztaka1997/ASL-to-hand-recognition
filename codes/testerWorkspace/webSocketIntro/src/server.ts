9a9a7d834fa8592066711f0e40d7800f0e0b94a8
Insignite <nh.minhkhanhLA@gmail.com>
2019-02-13 12:40:32 -0800

    Add broadcard to websocket. Currently fixing disconnect.

--- codes/testerWorkspace/webSocketIntro/src/server.ts
+++ codes/testerWorkspace/webSocketIntro/src/server.ts
@@ -10,7 +10,37 @@ const server = http.createServer(app);
 // Initialize the wbesocket server instance
 const wss = new WebSocket.Server({server});
 
-wss.on('connection', (ws: WebSocket) =>{
+wss.on('connection', (ws: ExtWebSocket) =>{
+    ws.isAlive = true;
+    
+
     // establish a connection 
     // Add simple event to test 
-})
+    ws.on('message', (message: string) =>{
+        // log the console file and send it back to client
+        console.log('receive: %s', message);
+
+        const broadcastRegex = /^broadcast\:/;
+
+        if(broadcastRegex.test(message)){
+            message = message.replace(broadcastRegex, '');
+
+            //send back the message to other clients
+            wss.clients.forEach(client =>{
+                if(client != ws){
+                    client.send('Hello, broadcast message -> ${message}');
+                }
+            });
+        }
+        else{
+            ws.send('Hello, you sent -> ${message}');
+        }
+    });
+
+    ws.send('Hi there, I am a WebSocket');
+});
+
+// Start server
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Server started on port 3000`);
+});
