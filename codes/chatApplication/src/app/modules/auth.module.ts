ffc79fb7bf4a46a96d1844b809b0a84e9985c2af
Khanh Nguyen <nh.minhkhanhLA@gmail.com>
2019-02-18 12:40:50 -0800

    Styling logIn and signUp page. Chaning default home page.

--- codes/chatApplication/src/app/modules/auth.module.ts
+++ codes/chatApplication/src/app/modules/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from '../components/authentication/authentication.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
 
 @NgModule({
-  declarations: [AuthenticationComponent],
+  declarations: [AuthenticationComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule],
-  exports: [AuthenticationComponent]
+  exports: [AuthenticationComponent, LoginComponent, RegisterComponent]
 })
 export class AuthModule {}
