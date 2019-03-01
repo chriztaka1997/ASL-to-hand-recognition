549fe2bd1c8902992c558168ccb9fee22a94eadf
ry-valdriz <38999021+ry-valdriz@users.noreply.github.com>
2019-02-28 17:38:57 -0800

    Delete audio.py

--- codes/videochat/audio.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-
-
-def callback(indata, frames, time, status):
-    print(frames)
-
-stream = sd.RawInputStream(channels=2, samplerate=48000, callback=callback)
-with stream:
-    sd.sleep(5 * 1000)
