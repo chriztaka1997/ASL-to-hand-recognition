42ad545dbfa463f74ad258686936526d3015db9a
Insignite <nh.minhkhanhLA@gmail.com>
2019-04-05 14:26:14 -0700

    Backup commit in case messing up. Note: Proceed with caution from this step

--- codes/chatApplication/src/app/modules/streams.module.ts
+++ codes/chatApplication/src/app/modules/streams.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StreamsComponent } from '../components/streams/streams.component';
 import { TokenService } from '../services/token.service';
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+import { SideComponent } from '../components/side/side.component';
 
 @NgModule({
-  declarations: [StreamsComponent],
+  declarations: [StreamsComponent, ToolbarComponent, SideComponent],
   imports: [CommonModule],
   exports: [StreamsComponent],
   providers: [TokenService]
